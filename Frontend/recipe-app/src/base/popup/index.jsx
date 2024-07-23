21b6b59f56dbb156f26739d7bb6cc72d52bf60d5
Majd <majdbitar2003@gmail.com>
2024-07-23 03:58:53 +0300

    added routing from login to home

--- Frontend/recipe-app/src/base/popup/index.jsx
+++ Frontend/recipe-app/src/base/popup/index.jsx
@@ -5,7 +5,7 @@ const Popup = ({ message, onClose }) => {
   return (
     <div className="popup-overlay">
       <div className="popup-content">
-        <h2>Popup</h2>
+        <h2>CAUTION</h2>
         <p>{message}</p>
         <button onClick={onClose}>Close</button>
       </div>
