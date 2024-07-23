e64ebd69dc1d2a018ab153a42034516409dfd9c5
Majd <majdbitar2003@gmail.com>
2024-07-23 04:28:07 +0300

    added NavBar component

--- Frontend/recipe-app/src/App.jsx
+++ Frontend/recipe-app/src/App.jsx
@@ -3,6 +3,7 @@ import Button from './base/Button';
 import Input from './base/Input';
 import Socials from './components/Socials';
 import LoginSignup from './pages/Login-Signup'
+import NavBar from './components/NavBar';
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { routes } from "./utils/routes.jsx";
@@ -13,7 +14,7 @@ const App = ()=>{
         <BrowserRouter>
         <Routes>
           <Route path={routes.login} element={<LoginSignup />} />
-          <Route path={routes.home} element={<div>HOME</div>} />
+          <Route path={routes.home} element={<NavBar></NavBar>} />
           <Route path="/*" element={<h3>Not Found</h3>} />
         </Routes>
       </BrowserRouter>
